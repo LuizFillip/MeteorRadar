2c58b981e1792b55e459b1e63e73a21b6753150a
LuizFillip <luizfillip@outlook.com>
2025-02-14 18:01:38 -0300

    updates

--- src/process.py
+++ src/process.py
@@ -1,7 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 12 11:33:59 2025
+import base as b 
+import spectral as sp
+import numpy as np 
 
-@author: Luiz
-"""
+year = 2022
 
+fname = f'MeteorRadar/data/proc/{year}'
+
+df = b.load(fname)
+
+df = df[(df["ht"] >= 88) & (df["ht"] <= 92)]
+
+
+df = df.resample('3H').mean().interpolate()
+# df = df.rolling('3H').mean().interpolate()
+
+df['doy'] = df.index.day_of_year + (df.index.hour / 24)
+
+
+parameter = 'merid'
+
+spec = sp.quick_plot(df, parameter, j1 = 5.5)
+
+spec.ax_sst.set(
+    ylabel = f'{parameter.title()} wind (m/s)',
+    ylim = [-200, 200]
+    )
+
+spec.ax_spec.set(yticks = np.arange(2, 12, 2))
